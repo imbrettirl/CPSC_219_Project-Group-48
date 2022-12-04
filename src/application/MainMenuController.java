ed5fa7170db747b4acfc0a83a3eb0c85c2a1ad62
Brett Gow <brett.gow@ucalgary.ca>
2022-12-04 07:15:27 +0000

    Added missed message to enemy

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -221,7 +221,12 @@ public class MainMenuController {
     		int enemyDamageDone = player.getHp() - damageEnemy;
     		player.setHp(damageEnemy);
     		playerHealthLabel.setText("Player Health: " + player.getHp());
-    		enemyMove.setText("Enemy did " + enemyDamageDone + " damage");
+    		if (enemyDamageDone > 0) {
+    			enemyMove.setText("Enemy did " + enemyDamageDone + " damage");
+    		} else {
+        		enemyMove.setText("Enemy attack missed, 0 damage taken");
+
+    		}
     	
     			if (player.getHp() <=0) {
     				enemyMove.setText("Enemy won!");
@@ -289,7 +294,12 @@ public class MainMenuController {
     	    		int enemyDamageDone = player.getHp() - damageEnemy;
     	    		player.setHp(damageEnemy);
     	    		playerHealthLabel.setText("Player Health: " + player.getHp());
-    	    		enemyMove.setText("Enemy did " + enemyDamageDone + " damage");
+    	    		if (enemyDamageDone > 0) {
+    	    			enemyMove.setText("Enemy did " + enemyDamageDone + " damage");
+    	    		} else {
+    	        		enemyMove.setText("Enemy attack missed, 0 damage taken");
+
+    	    		}
     	    	
     	    			if (player.getHp() <=0) {
     	    				enemyMove.setText("Enemy won!");
