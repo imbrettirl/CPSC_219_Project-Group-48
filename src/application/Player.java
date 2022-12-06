482adff014655a90cb3cb8ac6817190528f228b1
imbrettirl <114277284+imbrettirl@users.noreply.github.com>
2022-12-05 20:21:50 -0700

    Revert "Updating weapon to match master"

--- src/application/Player.java
+++ src/application/Player.java
@@ -7,11 +7,6 @@ public class Player {
 	private int ep;
 	private int damage;
 	private int energyDamage = 3;
-
-	private int hpCounter =0;
-	private int epCounter=0;
-	private int damageCounter=0;
-
 	int[] weapons = new int[3];
 	public String[] getPlayersWeapons() {
 	//String[] cart;
@@ -20,15 +15,12 @@ public class Player {
 	//look at indexes -> fill str
 	
 		return null;
-
+		
 	}
 	
 	Player(int health, int energy) {
 		setHp(health);
 		setEp(energy);
-		hpCounter =0;
-		epCounter=0;
-		damageCounter=0;
 	}
 
 	int getHp() {
@@ -53,7 +45,7 @@ public class Player {
 		if (dodgeChance > 1) {
 			Random r = new Random();
 			int rand = r.nextInt((5 - 1) + 1) + 1;
-			damage = rand + damageCounter;
+			damage = rand;
 			return damage;
 		}
 		else {
@@ -66,39 +58,6 @@ public class Player {
 	int getEnergyDamage() {
 		return energyDamage;
 	}
-
-	void energyUse() {
-		this.ep = this.ep - 5;
-	}
-	void healthUpgrade() {
-		this.hp = this.hp + 5;
-		hpCounter = getHpCounter() + 1;
-		System.out.print("counter = " +hpCounter);
-	}
-	void energyUpgrade() {
-		this.ep = this.ep + 5;
-		epCounter = getEpCounter() + 1;
-		System.out.print("counter = " +epCounter);
-	}
-	void attackUpgrade() {
-		this.damage = this.getDamage() + 1;
-		damageCounter = getDamageCounter() + 1;
-		System.out.print("counter = " +damageCounter);
-	}
-
-	int getHpCounter() {
-		return hpCounter;
-	}
-
-	int getEpCounter() {
-		return epCounter;
-	}
-
-	int getDamageCounter() {
-		return damageCounter;
-	}
-}
-
 	public void setWeapons(int[] weapons) {
 		this.weapons = weapons;
 	}
@@ -112,4 +71,3 @@ public class Player {
 	}
 
 }
-
